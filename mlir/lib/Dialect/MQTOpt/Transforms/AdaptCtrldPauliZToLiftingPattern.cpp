194852885da1dc641a36d9cb197f05a7e0e8d58c
Remme, Lian (lirem101) <lirem101@hhu.de>
2025-11-13 11:59:09 +0100

    :construction: starting to change Pauli Z targets

--- mlir/lib/Dialect/MQTOpt/Transforms/AdaptCtrldPauliZToLiftingPattern.cpp
+++ mlir/lib/Dialect/MQTOpt/Transforms/AdaptCtrldPauliZToLiftingPattern.cpp
@@ -10,6 +10,7 @@
 
 #include "mlir/Dialect/MQTOpt/IR/MQTOptDialect.h"
 
+#include <mlir/Dialect/Vector/IR/VectorOps.h>
 #include <mlir/IR/MLIRContext.h>
 #include <mlir/IR/PatternMatch.h>
 #include <mlir/Support/LogicalResult.h>
@@ -31,10 +32,107 @@ struct AdaptCtrldPauliZToLiftingPattern final
   explicit AdaptCtrldPauliZToLiftingPattern(mlir::MLIRContext* context)
       : OpInterfaceRewritePattern(context) {}
 
+  /**
+   * @brief This method checks if two gates are connected by the same qubits.
+   *
+   * This method checks if the output qubits of the first gate are exactly the
+   * input qubits of the second gate. There must be no qubit that is only used
+   * by one of the gates. The qubits may have different tasks (e.g. bein target
+   * in the first gate but ctrl in the second).
+   *
+   * @param firstGate The first unitary gate.
+   * @param secondGate The second unitary gate.
+   */
+  static bool areGatesConnectedBySameQubits(UnitaryInterface firstGate,
+                                            UnitaryInterface secondGate) {
+    auto inQubits = secondGate.getAllInQubits();
+    auto outQubits = firstGate.getAllOutQubits();
+
+    bool result = true;
+    result &= inQubits.size() == outQubits.size();
+    for (auto element : inQubits) {
+      result &= std::find(outQubits.begin(), outQubits.end(), element) !=
+                outQubits.end();
+    }
+    return result;
+  }
+
   mlir::LogicalResult
   matchAndRewrite(UnitaryInterface op,
                   mlir::PatternRewriter& rewriter) const override {
-    return mlir::failure();
+    // op needs to be a Pauli Z gate and controlled
+    std::string opName = op->getName().stripDialect().str();
+    if (opName != "z" || !op.isControlled()) {
+      return mlir::failure();
+    }
+
+    // op needs to be in front of a controlled hadamard gate
+    const auto& users = op->getUsers();
+    if (users.empty()) {
+      return mlir::failure();
+    }
+    auto user = *users.begin();
+    if (user->getName().stripDialect().str() != "h") {
+      return mlir::failure();
+    }
+
+    auto hadamardGate = mlir::dyn_cast<UnitaryInterface>(user);
+    if (!areGatesConnectedBySameQubits(op, hadamardGate)) {
+      return mlir::failure();
+    }
+
+    // If the target qubit of H is a ctrl in Z and vice versa, we can move Z's
+    // target to H's target
+
+    mlir::Value targetQubitHadamard = hadamardGate.getInQubits()[0];
+    mlir::Value targetQubitZ = op.getOutQubits()[0];
+    auto inCtrlHadamards = hadamardGate.getPosCtrlInQubits();
+    auto outCtrlPauliZ = op.getPosCtrlOutQubits();
+
+    bool zIsHadamardCtrlAndHadamardIsZCtrl =
+        std::find(inCtrlHadamards.begin(), inCtrlHadamards.end(),
+                  targetQubitZ) != inCtrlHadamards.end() &&
+        std::find(outCtrlPauliZ.begin(), outCtrlPauliZ.end(),
+                  targetQubitHadamard) != outCtrlPauliZ.end();
+
+    if (!zIsHadamardCtrlAndHadamardIsZCtrl) {
+      return mlir::failure();
+    }
+
+    auto targetQubitIn = op.getInQubits()[0];
+    auto targetQubitOut = op.getOutQubits()[0];
+
+    auto newTargetQubitInZ = op.getCorrespondingInput(targetQubitHadamard);
+
+    auto between = hadamardGate.getOutQubits()[0];
+
+    rewriter.replaceUsesWithIf(targetQubitIn, between,
+                               [&](mlir::OpOperand& operand) {
+                                 // We only replace the single use by the
+                                 // modified operation.
+                                 return operand.getOwner() == op;
+                               });
+    rewriter.replaceUsesWithIf(newTargetQubitInZ, targetQubitIn,
+                               [&](mlir::OpOperand& operand) {
+                                 // We only replace the single use by the
+                                 // modified operation.
+                                 return operand.getOwner() == op;
+                               });
+    rewriter.replaceUsesWithIf(between, newTargetQubitInZ,
+                               [&](mlir::OpOperand& operand) {
+                                 // We only replace the single use by the
+                                 // modified operation.
+                                 return operand.getOwner() == op;
+                               });
+
+    rewriter.replaceUsesWithIf(targetQubitHadamard, targetQubitOut,
+                               [&](mlir::OpOperand& operand) {
+                                 // We only replace the single use by the
+                                 // modified operation.
+                                 return operand.getOwner() == hadamardGate;
+                               });
+
+    return mlir::success();
   }
 };
 
