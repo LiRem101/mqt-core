ded10bc6cd14201263149b6e612556709157b68c
Remme, Lian (lirem101) <lirem101@hhu.de>
2025-12-10 10:52:18 +0100

    :construction: Added single, non-parametric gates.

--- mlir/include/mlir/Dialect/MQTOpt/Transforms/ConstantPropagation/GateToMap.h
+++ mlir/include/mlir/Dialect/MQTOpt/Transforms/ConstantPropagation/GateToMap.h
@@ -14,6 +14,9 @@ inline std::unordered_map<
     unsigned int, std::unordered_map<unsigned int, std::complex<double>>>
 getQubitMappingOfGates(qc::OpType gate) {
   switch (gate) {
+  case (qc::I):
+    return {{0, {{0, std::complex<double>(1, 0)}}},
+            {1, {{1, std::complex<double>(1, 0)}}}};
   case (qc::H):
     return {{0,
              {{0, std::complex<double>(1 / sqrt(2), 0)},
@@ -21,9 +24,55 @@ getQubitMappingOfGates(qc::OpType gate) {
             {1,
              {{0, std::complex<double>(1 / sqrt(2), 0)},
               {1, std::complex<double>(-1 / sqrt(2), 0)}}}};
+  case (qc::X):
+    return {{0, {{1, std::complex<double>(1, 0)}}},
+            {1, {{0, std::complex<double>(1, 0)}}}};
+  case (qc::Y):
+    return {{0, {{1, std::complex<double>(0, -1)}}},
+            {1, {{0, std::complex<double>(0, 1)}}}};
   case (qc::Z):
     return {{0, {{0, std::complex<double>(1, 0)}}},
             {1, {{1, std::complex<double>(-1, 0)}}}};
+  case (qc::S):
+    return {{0, {{0, std::complex<double>(1, 0)}}},
+            {1, {{1, std::complex<double>(0, 1)}}}};
+  case (qc::Sdg):
+    return {{0, {{0, std::complex<double>(1, 0)}}},
+            {1, {{1, std::complex<double>(0, -1)}}}};
+  case (qc::T):
+    return {{0, {{0, std::complex<double>(1, 0)}}},
+            {1, {{1, std::complex<double>(1 / sqrt(2), 1 / sqrt(2))}}}};
+  case (qc::Tdg):
+    return {{0, {{0, std::complex<double>(1, 0)}}},
+            {1, {{1, std::complex<double>(1 / sqrt(2), -1 / sqrt(2))}}}};
+  case (qc::V):
+    return {{0,
+             {{0, std::complex<double>(1 / sqrt(2), 0)},
+              {1, std::complex<double>(0, -1 / sqrt(2))}}},
+            {1,
+             {{0, std::complex<double>(0, -1 / sqrt(2))},
+              {1, std::complex<double>(-1 / sqrt(2), 0)}}}};
+  case (qc::Vdg):
+    return {{0,
+             {{0, std::complex<double>(1 / sqrt(2), 0)},
+              {1, std::complex<double>(0, 1 / sqrt(2))}}},
+            {1,
+             {{0, std::complex<double>(0, -1 / sqrt(2))},
+              {1, std::complex<double>(-1 / sqrt(2), 0)}}}};
+  case (qc::SX):
+    return {{0,
+             {{0, std::complex<double>(1 / 2, 1 / 2)},
+              {1, std::complex<double>(1 / 2, -1 / 2)}}},
+            {1,
+             {{0, std::complex<double>(1 / 2, -1 / 2)},
+              {1, std::complex<double>(1 / 2, 1 / 2)}}}};
+  case (qc::SXdg):
+    return {{0,
+             {{0, std::complex<double>(1 / 2, -1 / 2)},
+              {1, std::complex<double>(1 / 2, 1 / 2)}}},
+            {1,
+             {{0, std::complex<double>(1 / 2, 1 / 2)},
+              {1, std::complex<double>(1 / 2, -1 / 2)}}}};
   default:
     throw std::runtime_error(
         "Unsupported gate in mqt::ir::opt::qcp::getQubitMappingOfGates");
